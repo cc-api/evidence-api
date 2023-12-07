70d331d992eb7f7be751e7d2d55f46a55aa3d309
Lu Ken <ken.lu@intel.com>
2023-12-07 21:15:11 +0800

    cvm: add cli to retrieve TDREPORT (#5)
    Signed-off-by: Lu Ken <ken.lu@intel.com>

---
+++ common/python/cctrusted_base/tdx/common.py
@@ -0,0 +1,10 @@
+"""
+Common definition for Intel TDX.
+"""
+TDX_VERSION_1_0 = "1.0"
+TDX_VERSION_1_5 = "1.5"
+
+# The length of the reportdata
+TDX_REPORTDATA_LEN = 64
+# The length of the tdreport
+TDX_REPORT_LEN = 1024
--- vmsdk/python/cc_imr_cli.py
+++ vmsdk/python/cc_imr_cli.py
@@ -7,7 +7,7 @@ import cctrusted
 
 LOG = logging.getLogger(__name__)
 
-logging.basicConfig(level=logging.NOTSET)
+logging.basicConfig(level=logging.NOTSET, format='%(name)s %(levelname)-8s %(message)s')
 
 imr_inst = cctrusted.get_measurement([2, None])
 
