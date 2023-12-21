d729c05be2e5d4af208f34ba150ad66c9b90502b
Lu Ken <ken.lu@intel.com>
2023-12-21 05:46:50 +0000

    general: update the comments style
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- common/python/cctrusted_base/imr.py
+++ common/python/cctrusted_base/imr.py
@@ -6,9 +6,7 @@ from abc import ABC, abstractmethod
 from cctrusted_base.tcg import TcgDigest, TcgAlgorithmRegistry
 
 class TcgIMR(ABC):
-    """
-    Common Integrated Measurement Register class
-    """
+    """Common Integrated Measurement Register class."""
 
     _INVALID_IMR_INDEX = -1
 
@@ -19,21 +17,22 @@ class TcgIMR(ABC):
 
     @property
     def index(self) -> int:
-        """
-        The index of IMR register
-        """
+        """The index of IMR register."""
         return self._index
 
     @property
     def digests(self) -> dict:
-        """
-        Digests dict
-        """
+        """Digests dict."""
         return self._digests
 
     def digest(self, alg_id):
-        """
-        The digest value of IMR
+        """The digest value of IMR
+
+        Args:
+            alg_id: the alrogithms ID
+
+        Returns:
+            the digest according to given algorithms ID
         """
         if alg_id not in self._digests:
             return None
@@ -42,22 +41,24 @@ class TcgIMR(ABC):
     @property
     @abstractmethod
     def max_index(self):
-        """
-        The max index value of IMR
+        """The max index value of IMR
+
+        Different trust foundations may provides different count of IMR. For
+        example, TDX provides 4 IMR called RTMR, TPM provides 24 IMR called PCR.
         """
         raise NotImplementedError("Need implemented in different arch")
 
     def is_valid(self):
-        """
-        Check whether IMR is valid or not
+        """Check whether IMR is valid or not.
+
+        Returns:
+            whether IMR is valid or not.
         """
         return self._index != TcgIMR._INVALID_IMR_INDEX and \
             self._index <= self.max_index
 
 class TdxRTMR(TcgIMR):
-    """
-    RTMR class defined for Intel TDX
-    """
+    """RTMR class defined for Intel TDX."""
 
     @property
     def max_index(self):
@@ -68,9 +69,7 @@ class TdxRTMR(TcgIMR):
                         digest_hash)
 
 class TpmPCR(TcgIMR):
-    """
-    PCR class defined for TPM
-    """
+    """PCR class defined for TPM"""
 
     @property
     def max_index(self):
