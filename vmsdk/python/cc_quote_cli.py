21dd817a7987e5c4b56f876e6e92101ac8914b21
zhongjie <zhongjie.shi@intel.com>
2023-12-21 10:56:22 +0800

    vmsdk (python): rework example for get_quote
    Signed-off-by: zhongjie <zhongjie.shi@intel.com>

--- vmsdk/python/cc_quote_cli.py
+++ vmsdk/python/cc_quote_cli.py
@@ -9,29 +9,42 @@ LOG = logging.getLogger(__name__)
 OUT_FORMAT_RAW = "raw"
 OUT_FORMAT_HUMAN = "human"
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--out-format',
-    action='store',
-    help='Output format: raw/human. Default raw.',
-    dest='out_format'
-)
-args = parser.parse_args()
+def out_format_validator(out_format):
+    """Validator (callback for ArgumentParser) of output format
 
-dump_raw = False
-f = args.out_format
-if f is None:
-    # When the format is not set. Dump raw as default.
-    dump_raw = True
-elif f == OUT_FORMAT_HUMAN:
-    dump_raw = False
-elif f == OUT_FORMAT_RAW:
-    dump_raw = True
-else:
-    parser.print_help()
-    parser.exit(2, "Specified output format is not supported!")
+    Args:
+        out_format: User specified output format.
 
-logging.basicConfig(level=logging.NOTSET, format='%(name)s %(levelname)-8s %(message)s')
-quote = CCTrustedVmSdk.inst().get_quote(None, None, None)
-if quote is not None:
-    quote.dump(dump_raw)
+    Returns:
+        Validated value of the argument.
+
+    Raises:
+        ValueError: An invalid value is given by user.
+    """
+    if out_format not in (OUT_FORMAT_HUMAN, OUT_FORMAT_RAW):
+        raise ValueError
+    return out_format
+
+def main():
+    """Example to call get_quote and dump the result to stdout"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--out-format",
+        action="store",
+        default=OUT_FORMAT_RAW,
+        dest="out_format",
+        help="Output format: raw/human. Default raw.",
+        type=out_format_validator
+    )
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        level=logging.NOTSET,
+        format="%(name)s %(levelname)-8s %(message)s"
+    )
+    quote = CCTrustedVmSdk.inst().get_quote(None, None, None)
+    if quote is not None:
+        quote.dump(args.out_format == OUT_FORMAT_RAW)
+
+if __name__ == "__main__":
+    main()
