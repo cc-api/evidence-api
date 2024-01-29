8ef0057a8ea9b7c016840a4a877fc7885789a6e2
Zhongjie Shi <zhongjie.shi@intel.com>
2024-01-30 01:40:00 +0800

    vmsdk/python: add more test cases (#79)

--- vmsdk/python/tests/test_sdk.py
+++ vmsdk/python/tests/test_sdk.py
@@ -1,5 +1,8 @@
 """Containing unit test cases for sdk class"""
 
+from cctrusted_base.ccreport import CcReport, CcReportData, CcReportSignature
+from cctrusted_base.eventlog import EventLogs
+from cctrusted_base.tcg import TcgEfiSpecIdEvent, TcgImrEvent, TcgPcClientImrEvent
 import pytest
 
 def test_get_default_algorithms(vm_sdk, default_alg_id):
@@ -44,12 +47,50 @@ def test_get_cc_eventlog_with_invalid_input(vm_sdk):
         vm_sdk.get_cc_eventlog(start=0)
 
 def test_get_cc_eventlog_with_valid_input(vm_sdk):
-    """Test get_cc_eventlog() funtion with valid input."""
-    event_logs = vm_sdk.get_cc_eventlog()
-    assert event_logs is not None
+    """Test get_eventlog() funtion with valid input."""
+    eventlog = vm_sdk.get_cc_eventlog()
 
-def test_get_cc_report_with_valid_input(vm_sdk, check_quote):
+    # Check 1: the eventlog should not be None.
+    assert eventlog is not None
+
+    # Check 2: the object type should be correct.
+    assert isinstance(eventlog, EventLogs)
+    logs = eventlog.event_logs
+    assert logs is not None
+    assert isinstance(logs, list)
+    event_count = 0
+    for e in logs:
+        event_count += 1
+        assert isinstance(e, (TcgImrEvent, TcgPcClientImrEvent, TcgEfiSpecIdEvent))
+    assert event_count == eventlog.count
+
+def test_get_cc_report_with_valid_input(vm_sdk, check_quote_valid_input):
     """Test get_cc_report() function with valid input."""
     quote = vm_sdk.get_cc_report(None, None, None)
+
+    # Check 1: the quote should not be None.
     assert quote is not None
-    check_quote()
+
+    # Check 2: the object type should be correct.
+    assert isinstance(quote, CcReport)
+    quoted_data = quote.get_quoted_data()
+    assert quoted_data is not None
+    assert isinstance(quoted_data, CcReportData)
+    sigature_data = quote.get_sig()
+    assert sigature_data is not None
+    assert isinstance(sigature_data, CcReportSignature)
+
+    # Check 3: platform specific check.
+    check_quote_valid_input()
+
+def test_get_cc_report_with_invalid_input(check_quote_invalid_input):
+    """Test get_cc_report() function with invalid input."""
+    check_quote_invalid_input()
+
+def test_replay_cc_eventlog_with_valid_input(check_replay_eventlog_valid_input):
+    """Test replay_cc_eventlog() function with valid input."""
+    check_replay_eventlog_valid_input()
+
+def test_replay_cc_eventlog_with_invalid_input(check_replay_eventlog_invalid_input):
+    """Test replay_cc_eventlog() function with invalid input."""
+    check_replay_eventlog_invalid_input()
