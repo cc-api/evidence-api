9ffd12e664c470bfd85b119a0052e36130dd1c56
Zhongjie Shi <zhongjie.shi@intel.com>
2024-01-31 03:55:54 +0800

    vmsdk/python: refine sample cli (#87)

--- vmsdk/python/cc_quote_cli.py
+++ vmsdk/python/cc_quote_cli.py
@@ -4,8 +4,11 @@ Command line to get quote
 import argparse
 import base64
 import logging
+import os
 import random
-from cctrusted_vm import CCTrustedVmSdk
+from cctrusted_base.api import CCTrustedApi
+from cctrusted_vm.cvm import ConfidentialVM
+from cctrusted_vm.sdk import CCTrustedVmSdk
 
 LOG = logging.getLogger(__name__)
 OUT_FORMAT_RAW = "raw"
@@ -49,6 +52,13 @@ def make_userdata():
 
 def main():
     """Example to call get_cc_report and dump the result to stdout."""
+    if ConfidentialVM.detect_cc_type() == CCTrustedApi.TYPE_CC_NONE:
+        LOG.error("This is not a confidential VM!")
+        return
+    if os.geteuid() != 0:
+        LOG.error("Please run as root which is required for this example!")
+        return
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--out-format",
